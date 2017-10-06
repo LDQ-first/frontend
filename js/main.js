b4cb81586f2058feb98ad16254027e1240502933
LDQ-first <178848890@qq.com>
2017-10-06 17:07:31 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var items = document.querySelectorAll('x-markdown')
-console.log(items)
+/*console.log(items)*/
+
+marked.setOptions({
+    sanitize: true,
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value
+    }
+})
 
 Array.prototype.forEach.call(items, function (item) {
     var html = marked(item.textContent)
